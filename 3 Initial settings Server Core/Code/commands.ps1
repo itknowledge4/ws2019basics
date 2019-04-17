3cc2f22b0d27664c4626a6ef092fb716a583c63d
Adrian <dumitras.adrian@yahoo.com>
2019-04-17 22:07:43 +0300

    Added code to get Firewall rules for third video

--- 3 Initial settings Server Core/Code/commands.ps1
+++ 3 Initial settings Server Core/Code/commands.ps1
@@ -1,5 +1,9 @@
 #Enable firewall rules
 
+#You can check if they are enabled first with the following CmdLets
+Get-NetFirewallRule FPS-ICMP4-ERQ-In | Select-Object -ExpandProperty Enabled
+Get-NetFirewallRule FPS-SMB-In-TCP | Select-Object -ExpandProperty Enabled
+
 #Ping rule
 Enable-NetFirewallRule FPS-ICMP4-ERQ-In
 
